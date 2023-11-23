54fd59696df7751f2dd646e531d0b39388bde42c
idavid80 <dominguezbueno.david@gmail.com>
2023-11-23 09:46:15 +0100

    inicializando proyecto desde raiz

---
+++ src/main/java/com/api/api_vinos/ApiVinosApplication.java
@@ -0,0 +1,13 @@
+package com.api.api_vinos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiVinosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiVinosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api/api_vinos/ApiVinosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.api_vinos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiVinosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
